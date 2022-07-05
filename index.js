bbd36396fac16505d3c2812c54f70926bb5a1f88
Hamilton Junior <hamilton_jrribeiro@hotmail.com>
2022-07-05 16:37:35 -0300

    Upload de arquivos da quest

---
+++ index.js
@@ -0,0 +1,37 @@
+function somar (numero1, numero2) {
+    return (`O resultado da soma é: ${numero1 + numero2}`)
+}
+
+let resultadoDaSoma = somar (5, 5)
+
+console.log(resultadoDaSoma)
+
+/* ###################################################################### */
+
+function subtrair (numero1, numero2) {
+    return (`O resultado da subtração é: ${numero1 - numero2}`)
+}
+
+let resultadoDaSubtracao = subtrair (10, 5)
+
+console.log(resultadoDaSubtracao)
+
+/* ###################################################################### */
+
+function multiplicar (numero1, numero2) {
+    return (`O resultado da multiplicação é: ${numero1 * numero2}`)
+}
+
+let resultadoDaMultiplicacao = multiplicar (5, 10)
+
+console.log(resultadoDaMultiplicacao)
+
+/* ###################################################################### */
+
+function dividir (numero1, numero2) {
+    return (`O resultado da divisão é: ${numero1 / numero2}`)
+}
+
+let resultadoDaDivisao = dividir (10, 5)
+
+console.log(resultadoDaDivisao)
